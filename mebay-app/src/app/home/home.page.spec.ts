273d0b98dd67c8ba071d307464dd353ebfec5250
Roland Nagy <roland@cimem.hu>
2023-08-30 21:11:20 +0200

    feat(fe): setup jest

--- mebay-app/src/app/home/home.page.spec.ts
+++ mebay-app/src/app/home/home.page.spec.ts
@@ -1,15 +1,16 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { HomePage } from './home.page';
+import { IonicModule } from '@ionic/angular';
 
 describe('HomePage', () => {
   let component: HomePage;
   let fixture: ComponentFixture<HomePage>;
 
   beforeEach(async () => {
-    await TestBed
-      .configureTestingModule()
-      .compileComponents();
+    await TestBed.configureTestingModule({
+      imports: [IonicModule],
+    }).compileComponents();
 
     fixture = TestBed.createComponent(HomePage);
     component = fixture.componentInstance;
@@ -18,5 +19,19 @@ describe('HomePage', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should display First try', () => {
+    const compiled = fixture.nativeElement as HTMLElement;
+    expect(
+      compiled.querySelector('ion-toolbar ion-title')?.textContent
+    ).toContain('First try');
+  });
+
+  it('should not display Second try', () => {
+    const compiled = fixture.nativeElement as HTMLElement;
+    expect(
+      compiled.querySelector('ion-toolbar ion-title')?.textContent
+    ).not.toContain('Second try');
   });
 });
