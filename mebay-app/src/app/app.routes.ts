f52a065365fd52947a8824ad31bd7285da21796f
Roland Nagy <114018162+rolcho@users.noreply.github.com>
2023-09-01 17:19:36 +0200

    Registration page (#4)

--- mebay-app/src/app/app.routes.ts
+++ mebay-app/src/app/app.routes.ts
@@ -18,4 +18,8 @@ export const routes: Routes = [
     path: 'login',
     loadComponent: () => import('./pages/login/login.page').then( m => m.LoginPage)
   },
+  {
+    path: 'registration',
+    loadComponent: () => import('./pages/registration/registration.page').then( m => m.RegistrationPage)
+  },
 ];
